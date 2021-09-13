8ba1b2dc5ce2a9381924b25bb3e2d7a0d976a457
Will Henderson <14willh@gmail.com>
2021-09-14 00:51:28 +0100

    Add SAST runner

--- testing/sast/runner.js
+++ testing/sast/runner.js
@@ -1,6 +1,15 @@
+// 3rd party includes
+const cp = require('child_process');
+const path = require('path');
+
 const main = async () => 
 {
-    
+    // Install deps
+    cp.execSync("pip install njsscan", { stdio: 'ignore' });
+
+    // Run
+    const src_path = path.resolve(__dirname, '../../src');
+    cp.execSync(`njsscan ${src_path} --config .njsscan.yml`, { stdio: 'inherit' });
 }
 
 main();
